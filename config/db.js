a0e8596d589f233e79dfe05fbac689fa7b2dc580
Masum <masumc626@gmail.com>
2023-03-28 11:45:14 +0530

    added some new file

---
+++ config/db.js
@@ -0,0 +1,5 @@
+module.exports ={
+    'secret' : "10xauthsecret",
+    // 'db':"mongodb://localhost:27017/node-auth"
+    'db':"mongodb+srv://masumc626:mc9851981864@cluster0.inacp2q.mongodb.net/?retryWrites=true&w=majority"
+}
